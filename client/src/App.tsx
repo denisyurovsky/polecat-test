83c2b7a77e05895c96060738b352b87972a9a744
Denis Iurovskii <Denis_Iurovskii@epam.com>
2022-05-24 14:26:30 +0300

    fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,49 @@
 import "destyle.css";
-import React from "react";
+import React, {useEffect, useState} from "react";
+import './index.css'
 
 const App: React.FC = () => {
-  return <div>list of RATS That should stay away from my DAMN BINS</div>;
+
+    const [ratNames, setRatNames] = useState([])
+    const [ratInfo, setRatInfo] = useState<any>('')
+    const [name, setName] = useState('')
+
+    useEffect(() => {
+        fetch('http://localhost:7421/rat-names')
+            .then(data => data.json())
+            .then(data => setRatNames(data))
+    }, [])
+
+    useEffect(() => {
+        if (name) {
+            fetch(`http://localhost:7421/rat/${name}`)
+                .then(data => data.json())
+                .then(data => setRatInfo(data))
+        } else {
+            setRatInfo('')
+        }
+    }, [name])
+
+    return <div className='wrapper'>
+        <span>Choose rat</span>
+        <select onChange={(e) => setName(e.target.value)}>
+            <option value=''>
+                No Rat
+            </option>
+            {ratNames.map((name) => {
+                return <option key={name}>
+                    {name}
+                </option>
+            })}
+        </select>
+        { ratInfo &&
+            <div className='ratInfo'>
+            <span>width: {ratInfo.width || ''}</span>
+            <span>height: {ratInfo.height || ''}</span>
+            <span>nickname: {ratInfo.nickname || 'Uncool Rat with no Nickname'}</span>
+        </div>
+        }
+    </div>;
 };
 
 export default App;
